a07c40ea5683d642e65e03df5075297873f6fcce
Jonathas Santos de Resende <jonathasbombeiro@gmail.com>
2024-11-28 10:13:10 -0300

    criando cadastro de produto

--- estudando/backend/app.py
+++ estudando/backend/app.py
@@ -1,24 +1,51 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from db import get_connection
- 
+
 app = Flask(__name__)
 CORS(app)
- 
+
+# USERS
+@app.route("/users", methods=["GET"])
+def pegarUsuario():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return jsonify(users), 200
+
 @app.route("/users", methods=["POST"])
 def add_user():
-    data=request.json
-    conn=get_connection()
-    cursor=conn.cursor()
+    data = request.json
+    conn = get_connection()
+    cursor = conn.cursor()
     cursor.execute("""
     INSERT INTO users(nome)
-    values(%s)
-    """,(data["nome"],))
- 
+    VALUES(%s)
+    """, (data["nome"],))
     conn.commit()
     return jsonify({"message": "User created successfully"}), 201
- 
- 
- 
+
+# PRODUTOS
+@app.route("/produtos", methods=["POST"])
+def add_produto():
+    data = request.json
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    INSERT INTO produtos(nome,marca,valor)
+    VALUES(%s,%s,%s)
+    """, (data["nome"], data["marca"], data["valor"]))
+    conn.commit()
+    return jsonify({"message": "Product created successfully"}), 201
+
+@app.route("/produtos", methods=["GET"])
+def pegarProdutos():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM produtos")
+    products = cursor.fetchall()
+    return jsonify(products), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
--- estudando/frontend/script.js
+++ estudando/frontend/script.js
@@ -1,10 +1,31 @@
 const API_USER='http://127.0.0.1:5000/users'
  
 const formUser=document.querySelector("#form_user");
- 
+
+const corpoTabela=document.querySelector("#corpoTabela")
+
+
+
+async function buscarUsuario(){
+//essa é a nossa ligaçao com a API
+  const response =await fetch(API_USER);
+
+  //esse é o nosso json
+  const users = await response.json();
+
+  corpoTabela.innerHTML="";
+  users.array.forEach(element => {corpoTabela.innerHTML+= `
+  <th>
+  <td>${element.id}</td>
+  <td>${element.nome}</td>  
+  </th>
+  
+  `
+  })
+}
  
 formUser.addEventListener("submit",async (e)=>{
- 
+  e.preventDefault()
     const user={
         nome:document.querySelector("#name").value
     };
@@ -15,6 +36,6 @@ formUser.addEventListener("submit",async (e)=>{
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(user)
         });
-        fetchUsers();
+        buscarUsuario();
         formUser.reset();
 } ) // esse é o final do evento de submit
